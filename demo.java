859f7d197ed18f813d8fcbb074d8ad74ecdf7ba2
vignesh ramkumar <vignesh.ramkumar@outlook.com>
2026-06-09 21:45:48 +0530

    task-097 java file update

---
+++ demo.java
@@ -0,0 +1,6 @@
+create a helloworld program in javaHere is a simple "Hello, World!" program in Java:```java
+public class HelloWorld {      
+    public static void main(String[] args) { 
+        System.out.println("Hello, World!"); 
+    } 
+}
